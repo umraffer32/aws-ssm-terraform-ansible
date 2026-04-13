2ace7e099f8b3fc2fcec48cbfb24ba0e3bc86f83
Uriah Raffer <umraffer32@gmail.com>
2026-04-13 03:24:34 +0000

    adding instances/config

---
+++ compute.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "nat" {
+  tags = {
+    Name = "NAT"
+    Role = "ssm-nat"
+  }
+
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.nat_sg.id]
+  associate_public_ip_address = true
+  source_dest_check           = false
+  # key_name                    = var.key_name
+  iam_instance_profile        = "SSM-EC2"
+}
+
+resource "aws_instance" "ssm_host_1" {
+  tags = {
+    Name = "SSM-Host-1"
+    Role = "ssm-hosts"
+  }
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  # key_name               = var.key_name
+  iam_instance_profile   = "SSM-EC2"
+}
