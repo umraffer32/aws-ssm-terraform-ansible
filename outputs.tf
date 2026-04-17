82af0a0e03b682ce7205e16e88854e8ba3c423cb
Uriah Raffer <umraffer32@gmail.com>
2026-04-17 18:04:03 +0000

    re-organized outputs for SSM command

--- outputs.tf
+++ outputs.tf
@@ -3,16 +3,23 @@
 # }
 
 output "a-ssm_nat_command" {
-  value = "aws ssm start-session --target ${aws_instance.nat.id}"
+  value = "aws ssm start-session --target ${aws_instance.nat.id} - ${aws_instance.nat.tags["Name"]}"
 }
 
 # output "ssm_host_instance_ids" {
 #   value = aws_instance.ssm_hosts[*].id
 # }
 
+# output "ssm_host_ssm_commands" {
+#   value = [
+#     for id in aws_instance.ssm_hosts[*].id :
+#     "aws ssm start-session --target ${id} - ${aws_instance.nat.tags["Name"]}"
+#   ]
+# }
+
 output "ssm_host_ssm_commands" {
   value = [
-    for id in aws_instance.ssm_hosts[*].id :
-    "aws ssm start-session --target ${id}"
+    for i in aws_instance.ssm_hosts :
+    "aws ssm start-session --target ${i.id} - ${i.tags["Name"]}"
   ]
 }
