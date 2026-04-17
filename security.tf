16d5632ce211530f437dab40c75e317829a91f5e
Uriah Raffer <umraffer32@gmail.com>
2026-04-17 06:54:26 +0000

    removed SSH inbound rule

--- security.tf
+++ security.tf
@@ -11,20 +11,13 @@ resource "aws_security_group" "nat_sg" {
     security_groups = [aws_security_group.private_sg.id] 
   }
 
-  # ingress {
-  #   description = "Allow SSH from Main PC (for management)"
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = [var.admin_cidr]
-  # }
-
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -43,7 +36,10 @@ resource "aws_security_group" "private_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = [ "0.0.0.0/0" ]
+    # ipv6_cidr_blocks = ["::/0"]
   }
+
+
 
   tags = {
     Name = "SSM2-private-sg"
